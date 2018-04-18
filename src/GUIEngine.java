ab78bd5452b5cff73e794fdb29008a2744403a63
matia <matia@DESKTOP-MVQFLRA>
2018-04-18 00:22:25 -0400

    Text based navigable game world with random encounters.
    Some parts of the code need ot be rearranged for better encapsulation

---
+++ src/GUIEngine.java
@@ -0,0 +1,25 @@
+import javax.swing.JFrame;
+
+public class GUIEngine implements IUIEngine{
+	
+	public String getUserInput() {
+		return "";
+	}
+	
+	public void updateScreen(TileMap world, Player p) {
+		//
+	}
+	
+	public void displayDialogue(String d) {
+		//
+	}
+	
+	public boolean canMove(String direction, TileMap world, Player p) {
+		return true;
+	}
+	
+	public void movePlayer(String direction, Player p) {
+		//
+	}
+
+}
